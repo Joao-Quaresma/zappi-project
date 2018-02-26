887a8ef1f92aab16b592a0ba09e53c4995b12bb5
Joao Carlos dos Reis Quaresma da Conceicao Antonio <joao.quaresma1991@gmail.com>
2018-02-26 08:14:40 +0000

    add users table, update nav, add footer, istal gems

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root to: 'pages#index'
+
+  get 'contact', to: 'pages#contact'
+
+  get 'about', to: 'pages#about'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
